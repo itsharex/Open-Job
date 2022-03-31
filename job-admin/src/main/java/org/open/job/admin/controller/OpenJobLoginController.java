46c88b66511f3a21ffa1a12c212f0392a2d1eb15
李俊平 <2544054976@qq.com>
2022-03-31 14:44:55 +0800

    feat:优化登录认证逻辑

--- job-admin/src/main/java/org/open/job/admin/controller/OpenJobLoginController.java
+++ job-admin/src/main/java/org/open/job/admin/controller/OpenJobLoginController.java
@@ -1,11 +1,23 @@
 package org.open.job.admin.controller;
 
 import com.lightcode.starter.captcha.core.image.ImageValidateCode;
+import com.lightcode.starter.captcha.exception.ValidateCodeException;
 import com.lightcode.starter.captcha.processor.CaptchaProcessor;
 import com.lightcode.starter.captcha.request.CaptchaGenerateRequest;
+import com.lightcode.starter.captcha.request.CaptchaVerifyRequest;
+import com.lightcode.starter.oauth.core.password.PasswordAuthenticationProcessor;
+import com.lightcode.starter.oauth.core.sms.SmsMobileAuthenticationProcessor;
+import com.lightcode.starter.oauth.exception.AuthenticationException;
+import com.lightcode.starter.oauth.request.MobileLoginRequest;
+import com.lightcode.starter.oauth.request.PasswordLoginRequest;
+import com.lightcode.starter.oauth.token.AccessToken;
 import lombok.extern.slf4j.Slf4j;
 import org.open.job.admin.common.enums.ValidateCodeType;
 import org.open.job.admin.dto.req.OpenJobCaptchaRequest;
+import org.open.job.admin.dto.req.OpenJobMobileLoginRequest;
+import org.open.job.admin.dto.req.OpenJobPasswordLoginRequest;
+import org.open.job.common.exception.ControllerException;
+import org.open.job.common.vo.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -32,6 +44,13 @@ public class OpenJobLoginController {
 
     @Autowired
     private HttpServletResponse response;
+
+    @Autowired
+    private PasswordAuthenticationProcessor passwordAuthenticationProcessor;
+
+    @Autowired
+    private SmsMobileAuthenticationProcessor smsMobileAuthenticationProcessor;
+
     /**
      * 创建验证码
      */
@@ -58,5 +77,57 @@ public class OpenJobLoginController {
                     break;
             }
         });
+    }
+
+    /**
+     * 用户名密码登录
+     * @param request
+     * @return
+     */
+    @PostMapping("/account")
+    public Result<AccessToken> loginByUsername(@RequestBody @Valid OpenJobPasswordLoginRequest request){
+        CaptchaVerifyRequest captchaVerifyRequest = new CaptchaVerifyRequest()
+                .setRequestId(request.getDeviceId())
+                .setCode(request.getCaptcha());
+        try {
+            captchaProcessor.validate(captchaVerifyRequest);
+        } catch (ValidateCodeException e){
+            throw new ControllerException(e.getMessage());
+        }
+
+        PasswordLoginRequest passwordLoginRequest = new PasswordLoginRequest()
+                .setUsername(request.getUsername())
+                .setPassword(request.getPassword());
+        try {
+            final AccessToken accessToken = passwordAuthenticationProcessor.authentication(passwordLoginRequest);
+            return Result.succeed(accessToken);
+        } catch (AuthenticationException e){
+            throw new ControllerException(e.getMessage());
+        }
+    }
+
+    /**
+     * 手机号短信验证码登录
+     * @param request
+     * @return
+     */
+    @PostMapping("/mobile")
+    public Result<AccessToken> loginByMobile(@RequestBody @Valid OpenJobMobileLoginRequest request){
+        CaptchaVerifyRequest captchaVerifyRequest = new CaptchaVerifyRequest()
+                .setRequestId(request.getDeviceId())
+                .setCode(request.getCaptcha());
+        try {
+            captchaProcessor.validate(captchaVerifyRequest);
+        } catch (ValidateCodeException e){
+            throw new ControllerException(e.getMessage());
+        }
+
+        MobileLoginRequest mobileLoginRequest = new MobileLoginRequest().setMobile(request.getMobile());
+        try {
+            final AccessToken accessToken = smsMobileAuthenticationProcessor.authentication(mobileLoginRequest);
+            return Result.succeed(accessToken);
+        } catch (AuthenticationException e){
+            throw new ControllerException(e.getMessage());
+        }
     }
 }
